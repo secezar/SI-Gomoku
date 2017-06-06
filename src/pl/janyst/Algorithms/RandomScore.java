d47516d0511d81ce84b43ebc83b7d34289cbba63
secezar <piotrjanyst95@gmail.com>
2017-06-06 11:04:40 +0200

    Updated RandomScore

--- src/pl/janyst/Algorithms/RandomScore.java
+++ src/pl/janyst/Algorithms/RandomScore.java
@@ -2,6 +2,7 @@ package pl.janyst.Algorithms;
 
 import pl.janyst.Game.Gomoku;
 
+import java.util.HashSet;
 import java.util.Random;
 
 /**
@@ -9,9 +10,9 @@ import java.util.Random;
  */
 public class RandomScore implements EvaluationFunction {
     @Override
-    public int evaluate(Gomoku gomoku) {
+    public int evaluate(Gomoku gomoku, HashSet<int[]> moves) {
         Random random = new Random();
-        int score = random.nextInt(4^4);
+        int score = random.nextInt((int) Math.pow(4, 4));
         return score;
     }
 }
