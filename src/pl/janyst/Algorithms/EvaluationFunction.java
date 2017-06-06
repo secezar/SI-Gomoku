5eca2186c50177fbfc2e81f7d12f35871280b5c5
secezar <piotrjanyst95@gmail.com>
2017-06-06 11:06:10 +0200

    Changed signature of evaluation function in interface

--- src/pl/janyst/Algorithms/EvaluationFunction.java
+++ src/pl/janyst/Algorithms/EvaluationFunction.java
@@ -2,9 +2,11 @@ package pl.janyst.Algorithms;
 
 import pl.janyst.Game.Gomoku;
 
+import java.util.HashSet;
+
 /**
  * Created by Piotr Janyst on 2017-05-06.
  */
 public interface EvaluationFunction {
-    public int evaluate(Gomoku gomoku);
+    public int evaluate(Gomoku gomoku, HashSet<int[]> moves);
 }
